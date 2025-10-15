276c15efc8c77b552f40b43b2f0f0d9bafbd5965
NesteaBadApple <aikaichikawaa@gmail.com>
2025-10-15 20:38:44 +0800

    Completed
    Possible additions - ending scene/ new maps

--- Main/i_shall_illuminate/scenes/main_menu.gd
+++ Main/i_shall_illuminate/scenes/main_menu.gd
@@ -3,10 +3,6 @@ extends CanvasLayer
 @onready var start_button = $VBoxContainer/Start
 @onready var quit_button = $VBoxContainer/Quit
 
-func _ready():
-	start_button.connect("pressed", Callable(self, "_on_start_pressed"))
-	quit_button.connect("pressed", Callable(self, "_on_quit_pressed"))
-
 func _on_start_pressed():
 	# Load your main game scene
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
